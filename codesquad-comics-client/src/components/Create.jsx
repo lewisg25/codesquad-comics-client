43af501ef91809fc1543b1fc9b4f1d116af37c2d
Lewis G <lewis.garnett96@yahoo.com>
2026-04-12 17:09:56 -0400

    added local storage and useSet to components

--- codesquad-comics-client/src/components/Create.jsx
+++ codesquad-comics-client/src/components/Create.jsx
@@ -1,89 +1,28 @@
-// import React from "react";
-
-// const Create = () => {
-//     return (
-//         <>
-//           <main className="container">
-//         <h1>Create New Comic</h1>
-//         <p>Fill out the form below to add a new comic to the collection.</p>
-//         <form action="#" method="POST" class ="create-form">
-//             <div>
-//                 <label for="title">Title:</label>
-//                 <input type="text" id="title" name="title" placeholder="Title" required/>
-//             </div>
-//             <div>
-//                 <label for="author">Author:</label>
-//                 <input type="text" id="author" name="author" placeholder="Author" required/>
-//             </div>
-//             <div>
-//                 <label for="publisher">Publisher:</label>
-//                 <select id="publisher" name="publisher" required>
-//                     <option value="">Select</option>
-//                     <option value="BOOM! Box">BOOM! Box</option>
-//                     <option value="DC Comics">DC Comics</option>
-//                     <option value="Harry N. Abrams">Harry N. Abrams</option>
-//                     <option value="Icon Books">Icon Books</option>
-//                     <option value="Image Comics">Image Comics</option>
-//                     <option value="Marvel">Marvel</option>
-//                     <option value="Simon & Schuster">Simon & Schuster</option>
-//                     <option value="Top Shelf Productions">Top Shelf Productions</option>
-//                     <option value="VIZ Media LLC">VIZ Media LLC</option>
-//                 </select>
-//             </div>
-//             <div>
-//                 <label for="genre">Genre:</label>
-//                 <input type="text" id="genre" name="genre" placeholder="Enter genre" required/>
-//             </div>
-//             <div>
-//                 <label for="pages">Number of pages</label>
-//                 <input type="number" id="pages" name="pages" placeholder="Enter number of pages" required/>
-//             </div>
-//             <div>
-//                 <label for="rating">Rating:</label>
-//                 <input type="number" id="rating" name="rating" min="1" max="5" placeholder="" required/>
-//             </div>
-//             <div>
-//                 <label for="synopsis">Synopsis:</label>
-//                 <textarea id="synopsis" name="synopsis" placeholder="Enter comic synopsis" rows="4" cols="50"></textarea>
-//             </div>
-//             <button type="submit">Submit</button>
-//         </form>
-//     </main>
-//         </>
-//     )
-// }
-
-// export default Create
-
-
 const Create = () => {
+  const handleCreateSubmit = (e) => {
+    e.preventDefault();
+    console.log("Create form submission method ran");
 
-    const handleCreateSubmit = (e) => {
-        e.preventDefault();
-        console.log("Create form submission method ran");
-        
-        
-        console.log("Title:", e.target.title.value);
-        console.log("Author:", e.target.author.value);
-        console.log("Publisher:", e.target.publisher.value);
-        console.log("Genre:", e.target.genre.value);
-        console.log("Pages:", e.target.pages.value);
-        console.log("Rating:", e.target.rating.value);
-        console.log("Synopsis:", e.target.synopsis.value);
-    };
+    console.log("Title:", e.target.title.value);
+    console.log("Author:", e.target.author.value);
+    console.log("Publisher:", e.target.publisher.value);
+    console.log("Genre:", e.target.genre.value);
+    console.log("Pages:", e.target.pages.value);
+    console.log("Rating:", e.target.rating.value);
+    console.log("Synopsis:", e.target.synopsis.value);
+  };
 
-    return (
-        <main className="container">
-            <h1>Create New Comic</h1>
-            <form onSubmit={handleCreateSubmit} className ="create-form">
-                
-                <div>
-                    <label htmlFor="title">Title:</label>
-                    <input type="text" id="title" name="title" required/>
-                </div>
-    
-                <button type="submit">Submit</button>
-            </form>
-        </main>
-    )
-}
+  return (
+    <main className="container">
+      <h1>Create New Comic</h1>
+      <form onSubmit={handleCreateSubmit} className="create-form">
+        <div>
+          <label htmlFor="title">Title:</label>
+          <input type="text" id="title" name="title" required />
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  );
+};
--- codesquad-comics-client/src/components/Home.jsx
+++ codesquad-comics-client/src/components/Home.jsx
@@ -1,199 +1,44 @@
-import React from "react";
-import FunHomeLogo from '../../public/images/images/fun-home.jpg'
-import BatManLogo from '../../public/images/images/batman.jpg'
-import BlackPantherLogo from '../../public/images/images/black-panther.jpg'
-import HunterX from '../../public/images/images/hunter-x-hunter.jpg'
-import LumberJanes from '../../public/images/images/lumberjanes.jpg'
-import MarchLogo from '../../public/images/images/march.jpg'
-import OnePiece from '../../public/images/images/one-piece.jpg'
-import ParableLogo from '../../public/images/images/parable-of-the-sower.jpg'
-import QueerLogo from '../../public/images/images/queer.jpg'
-import TheWalkingDead from '../../public/images/images/the-walking-dead.jpg'
-import WakeLogo from '../../public/images/images/wake.jpg'
-import WatchMen from '../../public/images/images/watchmen.jpg'
-import booksData from '../data/books'
+import React, { useState, useEffect } from "react";
+import booksData from "../data/books";
+import CodeSquadlogo from "../../src/images/images/CodeSquadlogo.jpg";
 
 const Home = () => {
-    return (
-       <>
-         <div className="collection">
-        <div className="container">
-            <h1>CodeSquad Comics</h1>
-            <p>CodeSquad Comics is a collection of graphic novels read by Lewis Garnett. The site is intended to display
-                comic book covers along with information about each book, including the by, a rating, and other details
-                about the graphic novel. Browse through the complete collection below. Click on the cover image or the
-                Details link to see even more information about each graphic novel, including the publisher, genre, number
-                of pages, and a brief synopsis. The About page includes metadata about this collection. Login is only
-                available to the site administrator at this time.</p>
-        </div>
-            <div>
+  const [books, setBooks] = useState([]);
 
+  useEffect(() => {
+    setBooks(booksData);
+  }, []);
+
+  return (
+    <div className="collection">
+      <div className="container">
+        <h1>CodeSquad Comics</h1>
+        <p>
+          CodeSquad Comics is a collection of graphic novels read by Lewis
+          Garnett...
+        </p>
+      </div>
+      <div className="container">
+        <h1>Complete Comic Collection</h1>
+        <div className="comic-list">
+          {books.map((book) => (
+            <div className="comic-card" key={book.id}>
+              <a href="#">
+                <img src={book.image} alt={book.title} />
+              </a>
+              <h3>{book.title}</h3>
+              <p>by {book.author}</p>
+              <p>Rating: {book.rating}/5</p>
+              <a className="details-link" href="#">
+                Details
+              </a>
             </div>
-        <div className="container">
-            <div>
-                <h1>Complete Comic Collection</h1>
-            </div>
-            <div className="comic-list">
-                <div className="comic-card">
-                    <a href="#">
-                        <img src= {FunHomeLogo} alt="Fun Home"/>
-                    </a>
-                    <h3>Fun Home</h3>
-                    <p>by Alison Bechdel</p>
-                    <p>Rating: 5/5</p>
-                    <a className="details-link" href="#">Details</a>
-                </div>
-                <div className="comic-card">
-                    <a href="#">
-                        <img src={WatchMen} alt="Watchmen"/>
-                    </a>
-                    <h3>Watchmen</h3>
-                    <p>by Alan Moore</p>
-                    <p>Rating: 5/5</p>
-                    <a className="details-link" href="#">Details</a>
-                </div>
-                <div className="comic-card">
-                    <a href="#">
-                        <img src={HunterX} alt="Hunter x Hunter"/>
-                    </a>
-                    <h3>Hunter x Hunter</h3>
-                    <p>by Yoshihiro Togashi</p>
-                    <p>Rating: 4/5</p>
-                    <a className="details-link" href="#">Details</a>
-                </div>
-                <div className="comic-card">
-                    <a href="#">
-                        <img src={LumberJanes} alt="Lumberjanes"/>
-                    </a>
-                    <h3>Lumberjanes</h3>
-                    <p>by Noelle Stevenson</p>
-                    <p>Rating: 4/5</p>
-                    <a className="details-link" href="#">Details</a>
-                </div>
-                <div className="comic-card">
-                    <a href="#">
-                        <img src={OnePiece} alt="One Piece"/>
-                    </a>
-                    <h3>One Piece</h3>
-                    <p>by Eiichiro Oda</p>
-                    <p>Rating: 5/5</p>
-                    <a className="details-link" href="#">Details</a>
-                </div>
-                <div className="comic-card">
-                    <a href="#">
-                        <img src={WakeLogo} alt="Wake"/>
-                    </a>
-                    <h3>Wake</h3>
-                    <p>by Rebecca Hall</p>
-                    <p>Rating: 5/5</p>
-                    <a className="details-link" href="#">Details</a>
-                </div>
-                <div className="comic-card">
-                    <a href="black-panther.html">
-                        <img src={BlackPantherLogo} alt="Black Panther"/>
-                    </a>
-                    <h3>Black Panther</h3>
-                    <p>by Stan Lee</p>
-                    <p>Rating: 4/5</p>
-                    <a className="details-link" href="black-panther.html">Details</a>
-                </div>
-                <div className="comic-card">
-                    <a href="#">
-                        <img src={TheWalkingDead} alt="The Walking Dead"/>
-                    </a>
-                    <h3>The Walking Dead</h3>
-                    <p>by Robert Kirkman</p>
-                    <p>Rating: 4/5</p>
-                    <a className="details-link" href="#">Details</a>
-                </div>
-                <div className="comic-card">
-                    <a href="#">
-                        <img src={MarchLogo} alt="March"/>
-                    </a>
-                    <h3>March</h3>
-                    <p>by John Lewis</p>
-                    <p>Rating: 5/5</p>
-                    <a className="details-link" href="#">Details</a>
-                </div>
-                <div className="comic-card">
-                    <a href="#">
-                        <img src={BatManLogo} alt="Batman"/>
-                    </a>
-                    <h3>Batman</h3>
-                    <p>by Bob Kane</p>
-                    <p>Rating: 5/5</p>
-                    <a className="details-link" href="#">Details</a>
-                </div>
-                <div class="comic-card">
-                    <a href="#">
-                        <img src={QueerLogo} alt="Queer"/>
-                    </a>
-                    <h3>Queer</h3>
-                    <p>by Various</p>
-                    <p>Rating: 4/5</p>
-                    <a className="details-link" href="#">Details</a>
-                </div>
-                <div className="comic-card">
-                    <a href="#">
-                        <img src={ParableLogo} alt="Parable of the Sower"/>
-                    </a>
-                    <h3>Parable of the Sower</h3>
-                    <p>by Octavia Butler</p>
-                    <p>Rating: 4/5</p>
-                    <a className="details-link" href="#">Details</a>
-                </div>
-            </div>
-            <button>DISPLAY MORE</button>
+          ))}
         </div>
+        <button>DISPLAY MORE</button>
+      </div>
     </div>
-       </>
-      
-    )
-}
+  );
+};
 
 export default Home;
-
-
-// import React, { useState, useEffect } from "react";
-// import booksData from '../data/books';
-// import CodeSquadlogo from '../../src/images/images/CodeSquadlogo.jpg'
-
-
-// const Home = () => {
-   
-//     const [books, setBooks] = useState([]);
-
-   
-//     useEffect(() => {
-//         setBooks(booksData);
-//     }, []);
-
-//     return (
-//        <div className="collection">
-//         <div className="container">
-//             <h1>CodeSquad Comics</h1>
-//             <p>CodeSquad Comics is a collection of graphic novels read by Lewis Garnett...</p>
-//         </div>
-//         <div className="container">
-//             <h1>Complete Comic Collection</h1>
-//             <div className="comic-list">
-                
-//                 {books.map((book) => (
-//                     <div className="comic-card" key={book.id}>
-//                         <a href="#">
-//                             <img src={book.image} alt={book.title}/>
-//                         </a>
-//                         <h3>{book.title}</h3>
-//                         <p>by {book.author}</p>
-//                         <p>Rating: {book.rating}/5</p>
-//                         <a className="details-link" href="#">Details</a>
-//                     </div>
-//                 ))}
-//             </div>
-//             <button>DISPLAY MORE</button>
-//         </div>
-//     </div>
-//     )
-// }
-
-// export default Home;
